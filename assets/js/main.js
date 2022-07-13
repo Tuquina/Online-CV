5b27613b7adc3acc799c920418945f054bdba0a9
Tuquina <fernandotuquina@gmail.com>
2022-07-13 19:26:09 -0300

    Versión 1.0 Modificación HTML (datos finales) Agregado de CV provisional (móvil)

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,7 +108,7 @@ let opt = {
 
 // Function to call areaCv and Html2Pdf options 
 function generateResume(){
-    html12pdf(areaCv, opt)
+    html2pdf(areaCv, opt)
 }
 
 // When the button is clicked, it executes the three functions
